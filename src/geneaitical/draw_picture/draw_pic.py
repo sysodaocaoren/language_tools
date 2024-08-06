52cd474851985dcea7cd5b6bdfdeff964730dc88
Yumeng Zhu (朱玉猛) <zhuyumeng@inspur.com>
2024-08-06 20:35:56 +0800

    增加知乎的评论和回答的画图

---
+++ src/geneaitical/draw_picture/draw_pic.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageFont, ImageDraw
+import os
+import textwrap
+
+def w2p(text, name, cutline=True):
+    # 文字自动换行
+    if cutline:
+        sptext = text.split("\n")
+        hlen = len(sptext)
+        for i in range(hlen):
+            sptext[i] = "\n".join(textwrap.wrap(sptext[i], width=35))
+        text = "\n".join(sptext)
+
+    # 获取文字列表的最大字符数量
+    max_len = 0
+    for item in text.split("\n"):
+        if len(item) > max_len:
+            max_len = len(item)
+
+    # 转换文字为图片并保存为图片
+    h = 26 * len(text.split("\n")) + 10
+    w = int(max_len * 19.5 + 10)
+    im = Image.new("RGB", (w, h), (255, 255, 255))
+    dr = ImageDraw.Draw(im)
+    fpath = "D:\planself\workspace\language_tools\src\\xk_font.TTF"
+    font = ImageFont.truetype(fpath, 20)
+    dr.text((5, 5), text, font=font, fill="#000000")
+
+    # 存储图片到本地路径/w2p.png
+    save_path = os.getcwd()
+    if (os.path.exists(save_path)):
+        save_path = save_path + "/" + name +".png"
+    else:
+        os.mkdir(save_path)
+        save_path = save_path + "/" + name +".png"
+    im.save(save_path)
+    return save_path
+
+if __name__ == '__main__':
+    w2p("你真是个好人")
---
+++ src/geneaitical/draw_picture/zhihu_comment_top.py
@@ -0,0 +1,16 @@
+import sys
+import draw_pic
+sys.path.append("D:\planself\workspace\language_tools\src\geneaitical\splider\\utils")
+import db_mysql
+
+
+def draw_zhihu_top(keyword):
+    comment_list = db_mysql.getTopVoteComment("zhihu",5, keyword)
+    count = 0
+    for news_info in comment_list:
+        print(news_info)
+        draw_pic.w2p(news_info[0], "zhihu_cm_top" + str(count))
+        count = count + 1
+
+if __name__ == '__main__':
+    draw_zhihu_top("备注")
